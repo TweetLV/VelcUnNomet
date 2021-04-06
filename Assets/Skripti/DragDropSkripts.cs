29a93c627b7a8a1edca74f5e50b7e11ad2465fd1
TweetLV <gas3210@inbox.lv>
2021-04-06 11:44:00 +0300

    Uzsākta pirmā skripta rakstīšana

---
+++ Assets/Skripti/DragDropSkripts.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Importē, lai lietotu pointer darbību interfeisu
+//Papildus ir jāpārliecinās, ka projektā pie Canvas ir importēta EventSystem UI komponente.
+using UnityEngine.EventSystems;
+
+//Piesaista pointer interfeisu
+public class DragDropSkripts : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+   // public Objekti objektuSkripts;
+    private RectTransform transformacijuLogs;
+    private CanvasGroup kanvasGrupa;
+
+    private void Awake()
+    {
+        kanvasGrupa = GetComponent<CanvasGroup>();
+        transformacijuLogs = GetComponent<RectTransform>();
+    }
+    //Funkcija nostrādā, kad uzkliksķināts uz parvietojama objekta
+    public void OnPointerDown(PointerEventData notikums)
+    {
+        Debug.Log("Kreisais klikšķis uz parvietojamā objekta!");
+    }
+    //Funckija nostrādā uzsākot pārvietošanu
+    public void OnBeginDrag(PointerEventData notikums)
+    {
+        Debug.Log("Uzsakta parvietošana!");
+        kanvasGrupa.alpha = 0.6f;
+        kanvasGrupa.blocksRaycasts = false;
+    }
+
+    //Funkcija nostrādā pārvietošanas brīdī
+    public void OnDrag(PointerEventData notikums)
+    {
+        //transformacijuLogs.anchoredPosition += notikums.delta / objektuSkripts.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData notikums)
+    {
+        Debug.Log("Vilkšana pabeigta!");
+    }
+
+}
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # VelcUnNomet
 ## Veicamo darbu saraksts:
 - [x] Izveidot GitHub repo
-- [ ] Sagatavot Unity projektu darba uzsākšanai(mapes, bildes, skaņas)
-- [ ] Realizēt objektu vilkšanu un nomešanu(uz kartes, nepareizi, pareizi)
+- [x] Sagatavot Unity projektu darba uzsākšanai(mapes, bildes, skaņas)
+- [x] Realizēt objektu vilkšanu un nomešanu(uz kartes, nepareizi, pareizi)
 - [ ] Realizēt objektu transformācijas(izmēra maiņa, rotācija)
 - [ ] Realizēt laika atskaiti un uzvaras logu(laiks, zvaigznītes)
